438f967eddadeb4e6aa397b709d509cdfbe72d23
levi-sr93 <levi-sr@outlook.com>
2020-09-23 12:51:01 -0300

    Use multiple Context Inside One Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,36 @@
 import React from 'react'
 
 import { ThemeContext } from '../contexts/ThemeContext'
+import {AuthContext} from '../contexts/AuthContext'
 
 class Navbar extends React.Component {
   render() {
     // console.log(this.context)
     return (
-      <ThemeContext.Consumer>{(context) => {
-        console.log(context)
-        const {isDarkTheme, darkTheme, lightTheme} = context;
-        const theme =  isDarkTheme ? darkTheme : lightTheme;
-        
+      <AuthContext.Consumer>{(authContext) => {
         return (
-          <nav className="ui centered header" style={{background: theme.background, color: theme.text, height: '120px'}}>
-            <h2 style={{textAlign: 'center'}}>Levi</h2>
-              <div className="ui three buttons">
-                <button className="ui button">Overview</button>
-                <button className="ui button">Contact</button>
-                <button className="ui button">Support</button>
-              </div>
-          </nav>
+          <ThemeContext.Consumer>{(themeContext) => {
+            console.log(authContext)
+            const {isDarkTheme, darkTheme, lightTheme} = themeContext;
+            const {isLoggedIn, changeAuthStatus} = authContext;
+            const theme =  isDarkTheme ? darkTheme : lightTheme;
+            
+            return (
+              <nav className="ui centered header" style={{background: theme.background, color: theme.text, height: '120px'}}>
+                <h2 style={{textAlign: 'center'}}>Levi</h2>
+                <p onClick={changeAuthStatus}>{isLoggedIn ? 'logged in' : 'logged out'}</p>
+                  <div className="ui three buttons">
+                    <button className="ui button">Overview</button>
+                    <button className="ui button">Contact</button>
+                    <button className="ui button">Support</button>
+                  </div>
+              </nav>
+            )
+          }}
+          </ThemeContext.Consumer>
         )
       }}
-      </ThemeContext.Consumer>
+      </AuthContext.Consumer>
     );
   }
 };
