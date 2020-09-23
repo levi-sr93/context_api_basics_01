077fe5f56e4e2621d882a7b35f74488dae9e99b5
levi-sr93 <levi-sr@outlook.com>
2020-09-23 11:43:54 -0300

    Updating the context data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,28 @@ import React from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
 class Navbar extends React.Component {
-  static contextType = ThemeContext;
   render() {
     // console.log(this.context)
-    const {isDarkTheme, darkTheme, lightTheme} = this.context;
-    const theme =  isDarkTheme ? darkTheme : lightTheme;
     return (
-      <nav className="ui centered header" style={{background: theme.background, color: theme.text, height: '120px'}}>
-        <h2 style={{textAlign: 'center'}}>Levi</h2>
-        <div className="ui three buttons">
-          <button className="ui button">Overview</button>
-          <button className="ui button">Contact</button>
-          <button className="ui button">Support</button>
-        </div>
-      </nav>
-    )
+      <ThemeContext.Consumer>{(context) => {
+        console.log(context)
+        const {isDarkTheme, darkTheme, lightTheme} = context;
+        const theme =  isDarkTheme ? darkTheme : lightTheme;
+        
+        return (
+          <nav className="ui centered header" style={{background: theme.background, color: theme.text, height: '120px'}}>
+            <h2 style={{textAlign: 'center'}}>Levi</h2>
+              <div className="ui three buttons">
+                <button className="ui button">Overview</button>
+                <button className="ui button">Contact</button>
+                <button className="ui button">Support</button>
+              </div>
+          </nav>
+        )
+      }}
+      </ThemeContext.Consumer>
+    );
   }
-}
+};
 
-export default Navbar;
+export default Navbar;  
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import {ThemeContext} from '../contexts/ThemeContext'
 class TodoList extends React.Component {
-  static contextType = ThemeContext;
   render() {
-    //getting data from theme context
-    const {isDarkTheme, lightTheme, darkTheme} = this.context;
-    const theme = isDarkTheme ? darkTheme : lightTheme;
     return(
-      <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
-        <p className="item">study react</p>
-        <p className="item">drink coffee</p>
-        <p className="item">study Node</p>
-        <p className="item">Take a walk</p>
-      </div>
-    )
+      <ThemeContext.Consumer>{(context) => {
+        const {isDarkTheme, lightTheme, darkTheme, changeTheme } = context;
+        const theme = isDarkTheme ? darkTheme : lightTheme;
+        
+        return (
+          <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
+          <p className="item">study react</p>
+          <p className="item">drink coffee</p>
+          <p className="item">study Node</p>
+          <p className="item">Take a walk</p>
+
+          <button className='ui button primary' onClick={changeTheme}>Change Theme</button>
+        </div>
+        )
+      }}
+      </ThemeContext.Consumer>
+      )
   }
 }
 
