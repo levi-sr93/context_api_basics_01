893e2f65b4d4344a211cb0bed94e59cc64797751
levi-sr93 <levi-sr@outlook.com>
2020-09-23 12:23:09 -0300

    Creating Multiple Contexts

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import Navbar from './components/Navbar';
 import TodoList from './components/TodoList';
 
 import ThemeContextProvider from './contexts/ThemeContext';
+import AuthContextProvider from './contexts/AuthContext';
 
 
 function App() {
   return (
     <div className="App">
       <div className="ui raised padded text container segment">
-        <ThemeContextProvider>
-          <Navbar />
-          <TodoList />
-        </ThemeContextProvider>
+          <AuthContextProvider>
+            <ThemeContextProvider>
+              <Navbar />
+              <TodoList />
+            </ThemeContextProvider>
+          </AuthContextProvider>
       </div>
     </div>
   );
