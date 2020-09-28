81289242a45be784aa04cefac7f98774added1c1
levi-sr93 <levi-sr@outlook.com>
2020-09-28 14:46:46 -0300

    useContext hook - Refactoring from class to func

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {ThemeContext} from '../contexts/ThemeContext'
-class TodoList extends React.Component {
-  render() {
-    return(
-      <ThemeContext.Consumer>{(context) => {
-        const {isDarkTheme, lightTheme, darkTheme, changeTheme } = context;
-        const theme = isDarkTheme ? darkTheme : lightTheme;
+// class TodoList extends React.Component {
+//   render() {
+//     return(
+//       <ThemeContext.Consumer>{(context) => {
+//         const {isDarkTheme, lightTheme, darkTheme, changeTheme } = context;
+//         const theme = isDarkTheme ? darkTheme : lightTheme;
         
-        return (
-          <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
-          <p className="item">study react</p>
-          <p className="item">drink coffee</p>
-          <p className="item">study Node</p>
-          <p className="item">Take a walk</p>
+//         return (
+//           <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
+//           <p className="item">study react</p>
+//           <p className="item">drink coffee</p>
+//           <p className="item">study Node</p>
+//           <p className="item">Take a walk</p>
 
-          <button className='ui button primary' onClick={changeTheme}>Change Theme</button>
-        </div>
-        )
-      }}
-      </ThemeContext.Consumer>
-      )
-  }
+//           <button className='ui button primary' onClick={changeTheme}>Change Theme</button>
+//         </div>
+//         )
+//       }}
+//       </ThemeContext.Consumer>
+//       )
+//   }
+// }
+
+const TodoList = () => {
+  const {isDarkTheme, lightTheme, darkTheme, changeTheme } = useContext(ThemeContext);
+  const theme = isDarkTheme ? darkTheme : lightTheme;
+  
+  return (
+    <div style={{background: theme.background, color: theme.text, height: '140px', textAlign: 'center'}}>
+        <p className="item">study react</p>
+        <p className="item">drink coffee</p>
+        <p className="item">study Node</p>
+        <p className="item">Take a walk</p>
+
+        <button className='ui button primary' onClick={changeTheme}>Change Theme</button>
+    </div>
+  )
 }
 
 export default TodoList;
