9a79c52cf882d14f7affc51f8125affb7037b9a2
Levi <levi-sr@outlook.com>
2020-09-28 19:14:16 -0300

    Add New Todo Hooks and Context

--- src/contexts/TodoListContext.js
+++ src/contexts/TodoListContext.js
@@ -1,17 +1,24 @@
-import React, {useState, createContext} from 'react'
+import React, { useState, createContext } from 'react'
 
 
 export const TodoListContext = createContext();
 
-const TodoListContextProvider = ({children}) => {
+const TodoListContextProvider = ({ children }) => {
   const [todos, setTodos] = useState([
-    {text: 'Plan the family trip', id: 1},
-    {text: 'Go shopping for dinner', id: 2},
-    {text: 'Study React', id: 3}
+    { text: 'Plan the family trip', id: 1 },
+    { text: 'Go shopping for dinner', id: 2 },
+    { text: 'Study React', id: 3 }
   ]);
 
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { text: todo, id: Math.random() } 
+    ]);
+  }
+
   return (
-    <TodoListContext.Provider value={{ todos }}>
+    <TodoListContext.Provider value={{ todos, addTodo }}>
       {children}
     </TodoListContext.Provider>
   )
